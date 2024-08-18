c5f89e55338ca06bb6af66a0c1c29ff0b4f24f53
staian00 <gleb.ponasenkov@seznam.cz>
2024-08-18 20:26:47 +0200

    Adding data

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 from fpdf import FPDF
 import pandas as pd
 
+
+class FPDF(FPDF):
+    def footer(self):
+        # Go to 1.5 cm from bottom
+        self.set_y(-15)
+        # Select Arial italic 8
+        page_num = self.page_no()
+        # Print centered page number
+        self.cell(0, 10, str(page_num), 0, 0, 'C')
+
+
 df = pd.read_csv('topics.csv', sep=',')
 df.sort_values(by='Order', inplace=True)
-counter = 1
 count_rows = df.shape[0]
-print(df.iloc[0]['Pages'])
 
 pdf = FPDF(orientation='P', unit='mm', format='A4')
 # Adding topics
@@ -14,16 +23,11 @@ for i in range(count_rows):
     pdf.set_font(family='Times', style='B', size=20)
     pdf.cell(w=0, h=12, txt=f'{df.iloc[i]['Topic'].title()}', align='L', ln=1, border=0)
     pdf.line(10, 20, 200, 20)
-    pdf.set_font(family='Times', style='B', size=10)
-    pdf.ln(242)
-    pdf.cell(w=0, h=12, txt=f'{counter}', align='C', ln=1, border=0)
-    counter += 1
+    pdf.set_font(family='Times', style='I', size=10)
+    pdf.multi_cell(w=0, h=5, txt=f'{df.iloc[i]['Text']}', align='L', border=0)
     # Adding blank pages
     for page_count in range(df.iloc[0]['Pages'] - 1):
         pdf.add_page()
         pdf.set_font(family='Times', style='B', size=10)
-        pdf.ln(254)
-        pdf.cell(w=0, h=12, txt=f'{counter}', align='C', ln=1, border=0)
-        counter += 1
 
 pdf.output('output.pdf')
